fd8872538879c3c66dc9f3017d31ddcb27682f24
Mohamed Bakr <bakr.engineer@gmail.com>
2021-07-07 11:36:20 +0200

    init express router

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+// Init Routes
+// Routes files
+const authRoutes = require('./routes/v1/auth')
+const usersRoutes = require('./routes/v1/user')
+const bootcampsRoutes = require('./routes/v1/bootcamp')
+const coursesRoutes = require('./routes/v1/course')
+const reviewsRoutes = require('./routes/v1/review')
+// Mount routes
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/users', usersRoutes)
+app.use('/api/v1/bootcamps', bootcampsRoutes)
+app.use('/api/v1/courses', coursesRoutes)
+app.use('/api/v1/reviews', reviewsRoutes)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () =>
