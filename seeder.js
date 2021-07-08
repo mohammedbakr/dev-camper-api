75ed7663c7b8f5166b848c42641e61a9450b0755
Mohamed Bakr <bakr.engineer@gmail.com>
2021-07-08 17:50:59 +0200

    Seeder Database for bootcamps

---
+++ seeder.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const path = require('path')
+
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+// Load env vars
+dotenv.config({ path: './config/config.env' })
+
+// Load models
+const Bootcamp = require('./models/bootcamp')
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+
+// Load JSON files
+const bootcamp = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '_data', 'bootcamps.json'), 'utf-8')
+)
+
+// Import into Db
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamp)
+
+    console.log('Data Imported...')
+    process.exit(1)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany()
+
+    console.log('Data Destroyed...')
+    process.exit(1)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+if (process.argv[2] === '-i') {
+  importData()
+} else if (process.argv[2] === '-d') {
+  deleteData()
+}
