0d063688e3fcc04e97cf443001bd4b405e93bda2
Mohamed Bakr <bakr.engineer@gmail.com>
2021-07-09 17:31:11 +0200

    Add pagination

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -2,6 +2,7 @@ const Bootcamp = require('../models/bootcamp')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 const geocoder = require('../utils/geocoder')
+const { startSession } = require('../models/bootcamp')
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
@@ -13,7 +14,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query }
 
   // Fields to exclude
-  const removeFields = ['select', 'sort']
+  const removeFields = ['select', 'sort', 'page', 'limit']
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param])
@@ -41,12 +42,41 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt') // DESC createdAt
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 10
+  const startIndex = (page - 1) * limit // skip
+  const endIndex = page * limit
+  const total = await Bootcamp.countDocuments()
+
+  query = query.skip(startIndex).limit(limit)
+
   // Excuting query
   const bootcamps = await query
 
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps })
+  // Pagination result
+  const pagination = {}
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps
+  })
 })
 
 // @desc Create new bootcamp
